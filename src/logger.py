6ade6d2150b0925c0d1ae089450f0a412b47a032
orif <orif@ymail.com>
2018-10-23 23:12:41 +0500

    simple logger tool

---
+++ src/logger.py
@@ -0,0 +1,12 @@
+from time import strftime
+from typing import Any, Iterable, List, Type
+
+LOG_PATH = "./common.log"
+TIME_FMT = "%Y/%m/%d %H:%M:%S"
+
+
+def log(message: str, message_type: str):
+    line = "[{}] {}: {}".format(strftime(TIME_FMT), message_type, message)
+    output = open(LOG_PATH, "a")
+    output.write(line)
+    output.close()
