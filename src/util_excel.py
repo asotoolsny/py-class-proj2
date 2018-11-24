92b2d58c94d0ffb254d78776c72680756fd78127
AOS <aos@aos.com>
2018-11-24 15:20:58 -0500

    converting into objects

--- src/util_excel.py
+++ src/util_excel.py
@@ -1,60 +1,44 @@
 import openpyxl
 
+
+class ShippingInfo():
+    def __init__(self, row):
+        self.__row = row
+
+    @property
+    def first_name(self):
+        return self.__row["firstname"]
+
+    @property
+    def last_name(self):
+        return self.__row["lastname"]
+
+
 def read_row_values(row):
-    row_values=[]
+    row_values = []
     for cell in row:
         row_values.append(cell.value)
     return row_values
 
-def read_shipping_values():
-    wb = openpyxl.load_workbook("magento2.xlsx")
+
+def read_shipping_values(excel_path: str = "./input/magento2.xlsx"):
+    wb = openpyxl.load_workbook(excel_path)
     ws = wb.active
 
     header_row = ws[1]
     header_values = read_row_values(header_row)
 
-    shipping_info_list=[]
+    shipping_info_list = []
 
     for row in ws.iter_rows(min_row=2):
-        row_values=read_row_values(row)
+        row_values = read_row_values(row)
 
-        shipping_info={}
+        shipping_info = {}
 
-        for index in range(0,len(header_values)):
+        for index in range(0, len(header_values)):
             shipping_info[header_values[index]] = row_values[index]
-        shipping_info_list.append(shipping_info)
+
+        shipping_info_obj = ShippingInfo(shipping_info)
+        shipping_info_list.append(shipping_info_obj)
 
     return shipping_info_list
-
-
-
-# if ws["A1"].value == "username":
-#     username = ws["A2"].value
-
-# if ws["B1"].value == "firstname":
-#     firstname = ws["B2"].value
-
-# if ws["C1"].value == "lastname":
-#     lastname = ws["C2"].value
-
-# if ws["D1"].value == "company":
-#     company = ws["D2"].value
-
-# if ws["E1"].value == "street":
-#     street = ws["E2"].value
-
-# if ws["F1"].value == "city":
-#     city = ws["F2"].value
-
-# if ws["G1"].value == "state":
-#     state = ws["G2"].value
-
-# if ws["H1"].value == "postcode":
-#     postcode = ws["H2"].value
-
-# if ws["I1"].value == "country_id":
-#     country_id = ws["I2"].value
-
-# if ws["J1"].value == "telephone":
-#     phonenumber = ws["J2"].value
-
