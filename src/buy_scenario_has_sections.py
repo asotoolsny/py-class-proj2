0ccae3a817c3202ff4ca30039dda001136960c70
orif <orif@ymail.com>
2018-11-27 20:06:41 -0500

    added setup/teardown phases

--- src/buy_scenario_has_sections.py
+++ src/buy_scenario_has_sections.py
@@ -8,16 +8,19 @@ from utils import filter_by_text, stringify_links
 
 
 class TestHomePageSections(unittest.TestCase):
+    def setUp(self):
+        self.__driver = webdriver.Firefox()
+        self.__shop = ShopTester(self.__driver)
+
     def test_if_has_sections(self):
-        driver = webdriver.Firefox()
-        shop = ShopTester(driver)
-
-        home_page = shop.load_home_page()
-
+        home_page = self.__shop.load_home_page()
         section_links = home_page.section_links
-        self.assertIsNotNone(section_links)
 
-        shop._close()
+        assert section_links
+
+    def tearDown(self):
+        self.__shop._close()
+
 
 if __name__ == "__main__":
     unittest.main()
