db54e9d4a3c83a7ac68b5efecc2240b944654a80
Adyl <adil.khud@gmail.com>
2018-12-15 12:11:53 -0500

    firefox -> chrome

--- src/shop/ShopTester.py
+++ src/shop/ShopTester.py
@@ -1,8 +1,12 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-from .HomePage import HomePage
-from .WebDriverContainer import WebDriverContainer
+from HomePage import HomePage
+from ShopItemPage import ShopItemPage
+from ShopSearchPage import ShopSearchPage
+from ShopSectionPage import ShopSectionPage
+from ShopSubSectionPage import ShopSubSectionPage
+from WebDriverContainer import WebDriverContainer
 
 
 class ShopTester(WebDriverContainer):
@@ -15,3 +19,24 @@ class ShopTester(WebDriverContainer):
     def load_home_page(self):
         self._load_url(self._home_page_url)
         return HomePage(self._driver)
+
+    def load_section_page(self, section_link):
+        section_link.click()
+        return ShopSectionPage(self._driver)
+
+    def load_subsection_page(self, subsection_link):
+        subsection_link.click()
+        return ShopSubSectionPage(self._driver)
+
+    def load_item_page(self, item_link):
+        item_link.click()
+        return ShopItemPage(self._driver)
+
+    def search_item(self, value: str):
+        search_textbox = self._try_find_element(
+            self._search_textbox_selector, 20)
+        search_textbox.clear()
+        search_textbox.send_keys(value)
+        search_textbox.send_keys(Keys.ENTER)
+
+        return ShopSearchPage(self._driver)
